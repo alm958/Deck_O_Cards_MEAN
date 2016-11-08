9a9265c48ee61782c1895b484e19fd05e4414f51
alm958 <alm958@gmail.com>
2016-11-08 10:00:26 -0500

    update

--- cards.js
+++ cards.js
@@ -1,5 +1,9 @@
+
+$(document).ready(function(){
+
 const rank_set = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 const suit_set = ['S','C','H','D'];
+let discardlist = [];
 
 function CardConstructor(suit = suit_set[Math.floor(Math.random() * suit_set.length)], rank = rank_set[Math.floor(Math.random() * rank_set.length)]){
     this.suit = suit;
@@ -29,35 +33,70 @@ DeckConstructor.prototype.shuffle = function(){
   // While there remain elements to shuffle…
   for (m = this.deck.length - 1; m > 0; m--) {
     // Pick a remaining element…
-    i = Math.floor(Math.random() * m);
+    i = Math.floor(Math.random() * (m+1));
     // And swap it with the current element.
     [this.deck[m], this.deck[i]] = [this.deck[i], this.deck[m]];
   }
-  console.log('this.deck just before leaving shuffle()');
-  console.log(this.deck);
   return this;
 }
 
-let myFirstDeck = new DeckConstructor();
-let my2ndDeck = new DeckConstructor();
+function PlayerConstructor(name, initCardCount, deckName){
+    this.name = name;
+    this.hand = this.initializeHand(initCardCount, deckName);
+}
 
-console.log('myFirstDeck just after creation');
-console.log(myFirstDeck);
+PlayerConstructor.prototype.initializeHand = function(initCardCount, deckName){
+    let hand = [], i;
+    for (i = 0; i < initCardCount; i++){
+        hand[i] = deckName.deal();
+    }
+    return hand;
+}
 
+PlayerConstructor.prototype.updateHand = function(deckName, action = 'hit', discardIndicies = []){
+    if (action === 'hit'){
+        this.hand.push(deckName.deal())
+    }
+    if (action === 'draw'){
+        for (let i = 0; i < discardIndicies.length; i++){
+            this.hand.splice(i,1,deckName.deal());
+        }
+    }
+}
 
-console.log(myFirstDeck);
+let mydeck = new DeckConstructor();
 
-myFirstDeck.shuffle();
+mydeck.shuffle();
 
-console.log(myFirstDeck);
-console.log("my2ndDeck");
-console.log(my2ndDeck);
+$('.player').submit(function(){
+    var player = new PlayerConstructor($('input').val(), 5, mydeck);
+    $('#welcome').html('');
+    $('#welcome').append(`<h4>Welcome ${player.name}.  Your hand as delt is :`);
+    for (let i = 0; i < player.hand.length; i++){
+        $('#welcome').append(` <button id='${i}'>${player.hand[i].name}</button> `);
+    }
+    $('#welcome').append(`<h4>There are ${mydeck.deck.length} cards remaining in the deck.</h4><h4> You can discard as many as three cards from your hand.  Click on the cards above you would like to discard and hit the Draw button.</h4><form  class="draw" action="" method="post"><input class='draw' type="submit" name="Draw" value='Draw'></form>`);
+    return false;
+})
 
-var deltcard = my2ndDeck.deal();
-console.log(deltcard);
-console.log('2nd deak post deal');
-console.log(my2ndDeck);
-myFirstDeck.deal();
-console.log(myFirstDeck);
-myFirstDeck.deal();
-console.log(myFirstDeck);
+$(document.body).on('click', 'button' ,function(){
+    $(this).addClass('discard');
+    console.log($(this).attr('id'));
+    console.log(Number($(this).attr('id')));
+    discardlist.push(Number($(this).attr('id')))
+    console.log(discardlist);
+})
+
+$(document.body).on('click', '.draw' ,function(){
+    event.preventDefault();
+    player.updateHand(mydeck,'draw',discardlist)
+    console.log(player.hand);
+    $('#welcome').html('');
+    $('#welcome').append(`<h4>Welcome ${player.name}.  Your hand after your draw is :`);
+    for (let i = 0; i < player.hand.length; i++){
+        $('#welcome').append(` <button id='${i}'>${player.hand[i].name}</button> `);
+    }
+    $('#welcome').append(`<h4>There are ${mydeck.deck.length} cards remaining in the deck.</h4>`);
+})
+
+});
